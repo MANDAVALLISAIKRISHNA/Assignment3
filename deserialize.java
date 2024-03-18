f241ddd79c36968aad3f6e6b7a7728efbe65337b
MANDAVALLISAIKRISHNA <142582630+MANDAVALLISAIKRISHNA@users.noreply.github.com>
2024-03-18 20:13:16 +0530

    Add files via upload

--- deserialize.java
+++ deserialize.java
@@ -1,48 +1,84 @@
 import java.io.*;
-
 class Product implements Serializable {
     private int id;
     private String name;
     private String category;
     private double price;
-
     public Product(int id, String name, String category, double price) {
         this.id = id;
         this.name = name;
         this.category = category;
         this.price = price;
+    } 
+    public int getId() {
+        return id;
     }
 
-    public void displayProductDetails() {
-        System.out.println("Product ID: " + id);
-        System.out.println("Product Name: " + name);
-        System.out.println("Product Category: " + category);
-        System.out.println("Product Price: " + price);
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public static void main(String[] args) {
-        Product product = new Product(100, "phone", "Electronics", 8000.00);
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Category: " + category);
+        System.out.println("Price: $" + price);
+    }
+}
+public class deserialize {    
+    public static Product deserializeProduct(String fileName) {
         try {
-            FileOutputStream fileOut = new FileOutputStream("product.ser");
-            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-            objectOut.writeObject(product);
-            objectOut.close();
+            FileInputStream fileIn = new FileInputStream(fileName);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            Product product = (Product) in.readObject();
+            in.close();
+            fileIn.close();
+            return product;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        Product product = new Product(1010, "phone", "Electronics", 8888.88);
+        String fileName = "product.ser";
+        try {
+            FileOutputStream fileOut = new FileOutputStream(fileName);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(product);
+            out.close();
             fileOut.close();
-            System.out.println("serialized data ");
+            System.out.println("Product object serialized and saved to " + fileName);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try {
-            FileInputStream fileIn = new FileInputStream("product.ser");
-            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-            Product deserializedProduct = (Product) objectIn.readObject();
-            objectIn.close();
-            fileIn.close();
-            System.out.println("\nDeserialized  data:");
-            deserializedProduct.displayProductDetails();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+        System.out.println("\nDeserializing Product object from file...");
+        Product deserializedProduct = deserializeProduct(fileName);
+        if (deserializedProduct != null) {
+            System.out.println("\nDeserialized Product details:");
+            deserializedProduct.printDetails();
         }
     }
 }
